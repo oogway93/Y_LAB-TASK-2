8ab669b9c9ced509408aa5f2bc6a8419da05ce4f
oogway93 <marklem.coding.com>
2024-02-03 20:13:07 +0300

    in docker all works awesome!; facture of create_app

--- core.py
+++ core.py
@@ -3,26 +3,38 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title='Task 3', version='0.3.0')
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
+from config import REDIS_HOST
 
-re: redis.Redis = redis.Redis(host='localhost', port=6379)
+REDIS_HOST: str | None = REDIS_HOST
+
+# Проверяем, определена ли переменная REDIS_HOST
+if REDIS_HOST is not None:
+    # Переменная определена, инициализируем Redis с переданным хостом
+    re = redis.Redis(host=REDIS_HOST, port=6379)
+else:
+    # Переменная не определена, используем значение по умолчанию
+    re = redis.Redis(host='localhost', port=6379)
 
 
-@app.on_event('startup')
-async def on_startup():
+def create_app():
     from handlers.handlers_dish import router as dish_router
     from handlers.handlers_menu import router as menu_router
     from handlers.handlers_submenu import router as submenu_router
+    app = FastAPI(title='Task 3', version='0.3.0')
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
     app.include_router(menu_router)
     app.include_router(submenu_router)
     app.include_router(dish_router)
+    return app
+
+
+app = create_app()
 
 
 def main():
